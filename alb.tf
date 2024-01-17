f1eeec4e3fc8aac58c151177a93064bc7c85b53c
CodingManoj <CodingManoj@gmail.com>
2024-01-16 20:19:17 -0500

    TF ALB Auto Push

--- alb.tf
+++ alb.tf
@@ -13,3 +13,23 @@ resource "aws_lb" "alb" {
     Name = var.ALB_NAME
   }
 }
+
+
+# Creates listener for private alb
+resource "aws_lb_listener" "private" {
+  count             = var.INTERNAL ? 1 : 0
+
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Fixed response content"
+      status_code  = "200"
+    }
+  }
+}
